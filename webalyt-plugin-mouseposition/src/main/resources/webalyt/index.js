a3cd0be1121565e34cd68f2ab335c933ffd1dc28
petrfilip <mujfibi@gmail.com>
2017-12-30 14:06:22 +0100

    Improved javascript plugin style

--- webalyt-plugin-mouseposition/src/main/resources/webalyt/index.js
+++ webalyt-plugin-mouseposition/src/main/resources/webalyt/index.js
@@ -1,10 +1,25 @@
-
-var wpc_mouseposition = [];
-document.body.addEventListener('mousemove', function (e) {
-    var mp = {};
-    // mp.t = new Date();
-    mp.x = e.clientX;
-    mp.y = e.clientY;
-    wpc_mouseposition.push(mp);
-});
-
+var mousePositionPlugin = {
+    shortName: "mp",
+    fullName: "Mouse position plugin",
+    mousePositions: [],
+    methodBody: function () {
+        console.log(this.mousePositions.length);
+        document.body.addEventListener('mousemove', function (e) {
+            var mp = {};
+            mp.x = e.clientX;
+            mp.y = e.clientY;
+            mousePositionPlugin.mousePositions.push(mp);
+        });
+    },
+    onInit: function () {
+        this.mousePositions = [];
+    },
+    onAfterSend: function () {
+        this.onInit();
+    },
+    getDataForSending: function () {
+        return this.mousePositions;
+    }
+};
+mousePositionPlugin.onInit();
+webalyt.addPlugin(mousePositionPlugin);
--- webalyt-webserver/src/main/resources/webalyt/main.js
+++ webalyt-webserver/src/main/resources/webalyt/main.js
@@ -1,60 +1,62 @@
-//todo vyčlenit do modulu zvlášť -- je to core-webalyt d
-var wpo_fp = [];
-new Fingerprint2().get(function (result, components) {
-    var fpo = {};
-    fpo.fp = result;
-    //fpo.t = new Date();
-    wpo_fp.push(fpo);
-});
+var Webalyt =  function(webalytUrlTargetAddress) {
+
+    var wpo_fp = [];
+    new Fingerprint2().get(function (result, components) {
+        var fpo = {};
+        fpo.fp = result;
+        //fpo.t = new Date();
+        wpo_fp.push(fpo);
+    });
+
+    var webalytPlugins = [];
 
 
-//base method
-function mergeData(obj, plugin, message) {
-    if (message !== undefined && message.length > 0) {
-        obj[plugin] = message;
-    }
-    return obj;
-}
+    this.addPlugin = function (plugin) {
+        webalytPlugins.push(plugin);
+        plugin.methodBody();
+        plugin.onInit();
+    };
 
-//base method
-setInterval(function () {
-    var obj = {};
+    setInterval(function () {
+        var obj = {};
+        for (var i = 0; i < webalytPlugins.length; i++) {
+            obj = mergeData(obj, webalytPlugins[i].shortName, webalytPlugins[i].getDataForSending());
+        }
+        sendData(obj);
+        for (var j = 0; j < webalytPlugins.length; j++) {
+            webalytPlugins[j].onAfterSend();
+        }
+    }, 3000);
 
-    obj = mergeData(obj, "cp", wpc_click_tracker);
-    obj = mergeData(obj, "mp", wpc_mouseposition);
-    obj = mergeData(obj, "di", di);
-    obj = mergeData(obj, "lpi", lpi);
-    sendData(obj);
-}, 3000);
-
-
-//base method
-function sendData(obj) {
-    if (!isEmpty(obj)) {
-        //persistent field
-        obj = mergeData(obj, "WCdevId", wpo_fp);
-
-
-        var xhttp = new XMLHttpRequest();
-        xhttp.open("POST", "http://localhost:8080/", true);
-        xhttp.setRequestHeader("Content-type", "application/json");
-        var data = JSON.stringify(obj);
-        xhttp.send(data);
-
-        //non persistent-field
-        wpc_mouseposition = [];
-        wpc_click_tracker = [];
-        di = [];
-        lpi = [];
-    }
-}
-
-
-function isEmpty(obj) {
-    for (var prop in obj) {
-        if (obj.hasOwnProperty(prop))
-            return false;
+    //base method
+    function mergeData(obj, plugin, message) {
+        if (message !== undefined && message.length > 0) {
+            obj[plugin] = message;
+        }
+        return obj;
     }
 
-    return JSON.stringify(obj) === JSON.stringify({});
-}
+    //base method
+    function sendData(obj) {
+        if (!isEmpty(obj)) {
+            //persistent field
+            obj = mergeData(obj, "WCdevId", wpo_fp);
+
+            var xhttp = new XMLHttpRequest();
+            xhttp.open("POST", webalytUrlTargetAddress, true);
+            xhttp.setRequestHeader("Content-type", "application/json");
+            var data = JSON.stringify(obj);
+            xhttp.send(data);
+        }
+    }
+
+    function isEmpty(obj) {
+        for (var prop in obj) {
+            if (obj.hasOwnProperty(prop))
+                return false;
+        }
+        return JSON.stringify(obj) === JSON.stringify({});
+    }
+};
+
+var webalyt = new Webalyt("http://localhost:8080/");
