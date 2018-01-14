55343f75fabd33a0015cfa87ad499fe95e744a04
petrfilip <mujfibi@gmail.com>
2018-01-14 17:48:04 +0100

    Complete refactor

--- webalyt-analytics-session-player-base/src/main/java/cz/upce/webalyt/plugin/urlrecorder/repository/ClickRepository.java
+++
@@ -1,7 +0,0 @@
-package cz.upce.webalyt.plugin.urlrecorder.repository;
-
-import cz.upce.webalyt.plugin.core.WebalytBaseRepository;
-import cz.upce.webalyt.plugin.urlrecorder.entity.Click;
-
-public interface ClickRepository extends WebalytBaseRepository<Click> {
-}
